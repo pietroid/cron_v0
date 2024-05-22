fc43ad3181367a99191effd31e532e6aa8972a53
pietroid <pietrodomingues@gmail.com>
2024-05-21 22:06:03 -0300

    adding notification

---
+++ lib/notification_manager/notification_manager.dart
@@ -0,0 +1,34 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+
+class NotificationManager {
+  static final NotificationManager _instance = NotificationManager._internal();
+
+  factory NotificationManager() {
+    return _instance;
+  }
+
+  NotificationManager._internal();
+
+  void initialize() {
+    AwesomeNotifications().initialize(
+        // set the icon to null if you want to use the default app icon
+        'resource://drawable/res_app_icon',
+        [
+          NotificationChannel(
+              channelGroupKey: 'basic_channel_group',
+              channelKey: 'basic_channel',
+              channelName: 'Basic notifications',
+              channelDescription: 'Notification channel for basic tests',
+              defaultColor: const Color(0xFF9D50DD),
+              ledColor: Colors.white)
+        ],
+        // Channel groups are only visual and are not required
+        channelGroups: [
+          NotificationChannelGroup(
+              channelGroupKey: 'basic_channel_group',
+              channelGroupName: 'Basic group')
+        ],
+        debug: true);
+  }
+}
